61900d8f4383f556262ed76db94294517a26c4bb
Jason9789 <lifebook0809@gmail.com>
2022-04-13 19:20:50 +0900

    study: Create a  RESTful API server

--- RESTful/src/main.js
+++ RESTful/src/main.js
@@ -8,3 +8,106 @@
  * - 인증 로직은 넣지 않는다.
  * - RESTful API를 사용한다.
  */
+
+const http = require('http')
+
+/**
+ * @typedef Post
+ * @property {string} id
+ * @property {string} title
+ * @property {string} content
+ */
+
+/** @type {Post[]} */
+const posts = [
+  {
+    id: 'my_first_post',
+    title: 'My first post',
+    content: 'Hello!',
+  },
+  {
+    id: 'my_second_post',
+    title: '나의 두번째 포스트',
+    content: 'Hello Second!',
+  },
+]
+
+/**
+ *  Post
+ *
+ * GET /posts
+ * GET /posts/:id
+ * POST /posts
+ */
+
+const server = http.createServer((req, res) => {
+  const POSTS_ID_REGEX = /^\/posts\/([a-zA-Z0-9-_]+)$/
+  const postIdRegexResult =
+    (req.url && POSTS_ID_REGEX.exec(req.url)) || undefined
+
+  // GET /posts
+  if (req.url === '/posts' && req.method === 'GET') {
+    const result = {
+      posts: posts.map((post) => ({
+        id: post.id,
+        title: post.title,
+      })),
+
+      totalCount: posts.length,
+    }
+
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json; charset=utf-8')
+    res.end(JSON.stringify(result))
+  }
+  // GET /posts/:id
+  else if (postIdRegexResult && req.method === 'GET') {
+    const postId = postIdRegexResult[1]
+
+    const post = posts.find((_post) => _post.id === postId)
+
+    if (post) {
+      res.statusCode = 200
+      res.setHeader('Content-Type', 'application/json; charset=utf-8')
+      res.end(JSON.stringify(post))
+    } else {
+      res.statusCode = 404
+      res.end('Not Found')
+    }
+  }
+
+  // POST /posts
+  else if (req.url === '/posts' && req.method === 'POST') {
+    req.setEncoding('utf-8')
+    req.on('data', (data) => {
+      /**
+       * @typedef CreatePostBody
+       * @property {string} title
+       * @property {string} content
+       */
+
+      /** @type {CreatePostBody} */
+      const body = JSON.parse(data)
+
+      posts.push({
+        id: body.title.toLowerCase().replace(/\s/g, '_'),
+        title: body.title,
+        content: body.content,
+      })
+    })
+    res.statusCode = 200
+    res.end('Creating post')
+  }
+
+  // else
+  else {
+    res.statusCode = 404
+    res.end('Not Found.')
+  }
+})
+
+const PORT = 4000
+
+server.listen(PORT, () => {
+  console.log(`The server is listening at port: ${PORT}`)
+})
