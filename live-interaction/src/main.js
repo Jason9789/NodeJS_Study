cc74077e7466e531b99e370ff75934fdd074f9c3
Jason9789 <lifebook0809@gmail.com>
2022-04-19 15:01:09 +0900

    add: webSocket and live chat

--- live-interaction/src/main.js
+++ live-interaction/src/main.js
@@ -4,10 +4,14 @@
 // CSS framework: TailwindCSS
 
 const Koa = require('koa')
+const route = require('koa-route')
+const websockify = require('koa-websocket')
+const serve = require('koa-static')
+const mount = require('koa-mount')
 const path = require('path')
 const Pug = require('koa-pug')
 
-const app = new Koa()
+const app = websockify(new Koa())
 
 // @ts-ignore
 // eslint-disable-next-line no-new
@@ -16,9 +20,40 @@ new Pug({
   app,
 })
 
+app.use(mount('/public', serve('src/public')))
+
 app.use(async (ctx) => {
-  ctx.body = 'Hello World!'
   await ctx.render('main')
 })
+
+app.ws.use(
+  route.all('/ws', (ctx) => {
+    ctx.websocket.on('message', (data) => {
+      // console.log(`${message}`)
+      if (typeof data !== 'object') {
+        console.log('RETURN')
+        return
+      }
+
+      // @ts-ignore
+      const { message, nickname } = JSON.parse(data)
+
+      const { server } = app.ws
+
+      if (!server) {
+        return
+      }
+
+      server.clients.forEach((client) => {
+        client.send(
+          JSON.stringify({
+            message,
+            nickname,
+          })
+        )
+      })
+    })
+  })
+)
 
 app.listen(8000)
