9317f60427e89268d98c908a3178928f145881d2
Jason9789 <lifebook0809@gmail.com>
2022-04-19 12:58:59 +0900

    setup: pug, tailwindcss

--- live-interaction/src/main.js
+++ live-interaction/src/main.js
@@ -1,11 +1,24 @@
 // @ts-check
 
+// Template engine: Pug
+// CSS framework: TailwindCSS
+
 const Koa = require('koa')
+const path = require('path')
+const Pug = require('koa-pug')
 
 const app = new Koa()
 
+// @ts-ignore
+// eslint-disable-next-line no-new
+new Pug({
+  viewPath: path.resolve(__dirname, './views'),
+  app,
+})
+
 app.use(async (ctx) => {
   ctx.body = 'Hello World!'
+  await ctx.render('main')
 })
 
 app.listen(8000)
