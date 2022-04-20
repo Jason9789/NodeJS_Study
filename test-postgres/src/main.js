095e30b2c07fa181021268a50f001ef38dc23482
Jason9789 <lifebook0809@gmail.com>
2022-04-20 11:44:53 +0900

    hold

--- test-postgres/src/main.js
+++ test-postgres/src/main.js
@@ -1,59 +1,56 @@
 // @ts-check
 
-const { Client } = require('pg')
-const program = require('commander')
-const prompts = require('prompts')
+const { Sequelize, DataTypes } = require('sequelize')
 
-async function connect() {
-  const client = new Client({
-    user: 'myuser',
-    password: 'mypass',
+async function main() {
+  const sequelize = new Sequelize({
     database: 'fc21',
+    username: 'myuser',
+    password: 'mypass',
+    dialect: 'postgres',
+    host: 'localhost',
   })
 
-  await client.connect()
-  return client
+  const User = sequelize.define(
+    'user',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      age: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  )
+
+  const City = sequelize.define('city', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  })
+
+  User.belongsTo(City)
+
+  await sequelize.sync({
+    alter: true,
+  })
+
+  await sequelize.authenticate()
+  await sequelize.close()
 }
 
-program.command('list').action(async () => {
-  const client = await connect()
-
-  const query = `SELECT * FROM users`
-  const result = await client.query(query)
-  console.log(result.rows)
-
-  await client.end()
-})
-
-program.command('add').action(async () => {
-  const client = await connect()
-  const userName = await prompts({
-    type: 'text',
-    name: 'userName',
-    message: 'Provide a user name to insert.',
-  })
-
-  const query = `INSERT INTO users (name) VALUES ($1::text)`
-  await client.query(query, [userName.userName])
-
-  await client.end()
-})
-
-program.command('remove').action(async () => {
-  const client = await connect()
-
-  const userName = await prompts({
-    type: 'text',
-    name: 'userName',
-    message: 'Provide a user name to insert.',
-  })
-
-  // 무조건 고정된 String을 사용할 것. -> 그렇지 않으면 SQL Injection에 무방비 노출됨.
-  await client.query(`DELETE FROM users WHERE name = $1::text`, [
-    userName.userName,
-  ])
-
-  await client.end()
-})
-
-program.parseAsync()
+main()
